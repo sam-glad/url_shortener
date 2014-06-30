e7dba3c2a5d9685efa0dab5682a5ff60557c03df
Sam Gladstone <gladston@umich.edu>
2014-06-30 14:17:45 -0400

    Added pry; style changes

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 # server.rb
-# TODO add about page (erb file + content)
-# TODO automatically delete all links > 6 months old from database
+# TODO: add about page (erb file + content)
+# TODO: automatically delete all links > 6 months old from database
 
 require 'sinatra'
 require 'pg'
@@ -8,7 +8,7 @@ require 'pry'
 require './helper_methods.rb'
 
 get '/' do
-  @title = "SaZa"
+  @title = 'SaZa'
   @page_title = @title
   @urls = load_urls
   @count = @urls.length
@@ -17,18 +17,17 @@ end
 
 post '/' do
   @shrunken = assign_short_url
-  post_url(params["url"], @shrunken)
+  post_url(params['url'], @shrunken)
   redirect '/'
-end
-
-get '/about' do
-  'Foobar!'
-  erb :about
 end
 
 get '/:url_num' do # url_num being the @shrunken in the above "post" block
   @page_id = params[:url_num]
   @redir = locate_url(@page_id)
-  redirect "http://#{@redir.first["url"]}"
+  redirect "http://#{@redir.first['url']}"
 end
 
+get '/about' do
+  'Under construction...'
+  erb :about
+end
